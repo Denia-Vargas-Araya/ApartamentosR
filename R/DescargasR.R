3e62de9946efb5b6545ce431972f5cc790dae673
Denia-Vargas-Araya <54045477+Denia-Vargas-Araya@users.noreply.github.com>
2019-08-14 11:45:11 -0600

    actualizacion de dependencias
    Se añadieron los paquetes:
    +rvest
    +stringr
    +xml2

--- R/DescargasR.R
+++ R/DescargasR.R
@@ -8,9 +8,15 @@ pagina <- read_html("https://www.encuentra24.com/costa-rica-es/searchresult/bien
 
 #xpath (estiquetas)
 pagina %>% 
-  html_node(xpath = ".//div[@itemprop= 'price']") %>%
-  html_text(trim = TRUE)
+  html_node(xpath = "//div[@itemprop= 'price']") %>%
+  html_text(trim = TRUE) ->precio
+#cargar todos los precios en pagina actual
+pagina %>% 
+  html_nodes(xpath = "//div[@itemprop= 'price']") %>%
+  html_text(trim = TRUE) -> precios
+#cosas que debo buscar para la vida
+#¿que es html, que es xpath, que son las expresiones regulares?
 
-
+str_extract(precios, pattern = "\\d+(\\, \\d+\\.\\d+")
 
 
